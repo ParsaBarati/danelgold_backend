b894222a56ffefa69e68612e607da1fe846717d0
MoeinSS <moinranjbar905@gmail.com>
2024-10-13 11:07:57 +0330

    trying for connecting to server

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -3,9 +3,15 @@ import { DataSource } from 'typeorm';
 
 configDotenv({ path: '.env' });
 
+console.log('Database Host:', process.env.PG_HOST);
+console.log('Postgres Port:', process.env.PORT_PG_DB);
+console.log('Database:', process.env.DATABASE_PG_DB);
+console.log('Username:', process.env.USERNAME_PG_DB);
+console.log('Password:', process.env.PASSWORD_PG_DB);
+
 const datasource = new DataSource({
   type: 'postgres',
-  host: process.env.DATABASE_URL,
+  host: process.env.PG_HOST,
   port: parseInt(process.env.PORT_PG_DB, 10),
   database: process.env.DATABASE_PG_DB,
   username: process.env.USERNAME_PG_DB,
