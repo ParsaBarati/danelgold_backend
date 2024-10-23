d97f6d9fb117fb103b723f3a9e01fcb9ad5a71f2
Parsa Barati <parsabarati83@gmail.com>
2024-10-23 11:32:46 +0330

    fix upload and wallet

--- src/Social/Message/entity/message.entity.ts
+++ src/Social/Message/entity/message.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, ManyToOne, Column, CreateDateColumn, Relation } from 'typeorm';
+import {Entity, PrimaryGeneratedColumn, ManyToOne, Column, CreateDateColumn, Relation, OneToMany} from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 import { User } from '@/User/user/entity/user.entity';
 
@@ -20,5 +20,21 @@ export class Message {
   @ManyToOne(() => User, (receiver) => receiver.receivedMessages)
   @ApiProperty({ type: () => User })
   receiver: Relation<User>;
-  
+
+  // For story reply (null if not a story reply)
+  @Column({ type: 'int', nullable: true })
+  storyId: number;
+
+  // For post sharing (null if not a post sharing)
+  @Column({ type: 'int', nullable: true })
+  postId: number;
+
+  // For message reply (null if not a message reply)
+  @Column({ type: 'int', nullable: true })
+  replyId: number;
+
+  // For shared stories or posts (null if not shared)
+  @Column({ type: 'boolean', default: false })
+  isShared: boolean;
+
 }
